8f0d903caf64ce5cc2bc01403890eab538cc5356
sondesGas <gasmisondes001@gmail.com>
2021-10-28 11:44:27 +0100

    finalProps

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import Profile from "./profile/Profile";
 import image from "./profile/img.jpg";
 
 function App() {
-  const handleClick = (name) => alert(name);
+  const showAlert = (name) => alert(user.fullNAME);
+  const user = {
+    fullNAME: "GASMI SONDES ",
+    bio: "Full-stack Web Developer",
+    profession: `I design and code beautifully simple things,and I love what I do.In
+    addition to my engineering training in communications and networks, I
+    wanted to enhance my skills and knowledge by full-stack formation
+    within Go my code.`,
+  };
   return (
     <div className="App">
-      <Profile props={Profile} handleClick={handleClick}>
-        <img src={image} alt="..." />
+      <Profile user={user} showAlert={showAlert}>
+        <img src={image} alt="myPhoto" />
       </Profile>
     </div>
   );
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,26 +2,27 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PropTypes from "prop-types";
 
-const Profile = ({ props, children, handleClick }) => {
+const Profile = ({
+  user: { fullNAME, bio, profession },
+  showAlert,
+  children,
+}) => {
+  // console.log(props);
   return (
     <div className="card" style={myStyle}>
       <h3 className="card-header" style={{ color: "blue" }}>
-        GASMI SONDES <span style={{ marginLeft: 300 }}>{children}</span>
+        {fullNAME}
+        <span style={{ marginLeft: 300 }}>{children}</span>
       </h3>
       <div className="card-body">
-        <h5 className="card-title">Full-stack Web Developer </h5>
-        <p className="card-text">
-          I design and code beautifully simple things,and I love what I do.In
-          addition to my engineering training in communications and networks, I
-          wanted to enhance my skills and knowledge by full-stack formation
-          within Go my code.
-        </p>
+        <h5 className="card-title">{bio}</h5>
+        <p className="card-text">{profession}</p>
         <a
-          href="#"
+          href="#!"
           className="btn btn-primary"
           onClick={(event) => {
             event.preventDefault();
-            handleClick("GASMI SONDES");
+            showAlert();
           }}
         >
           It's me
@@ -30,8 +31,14 @@ const Profile = ({ props, children, handleClick }) => {
     </div>
   );
 };
-children.propTypes = { children: PropTypes.object.isRequired };
-Profile.defaultProps = { children: "Hi it's me" };
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+  showAlert: PropTypes.func.isRequired,
+};
+
+Profile.defaultProps = {
+  user: { fullNAME: "admin", bio: "admin", profession: "admin" },
+};
 const myStyle = { width: "50vw", marginLeft: 300 };
 
 export default Profile;
